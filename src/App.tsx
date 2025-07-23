cc09417f994e9eedbf9523e1c30f3f65703420df
Silantiev Jan <Silantiev-999@mail.ru>
2025-07-23 23:00:58 +0300

    relis

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.scss";
+import ky from "ky";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Post {
+  id: number;
+  title: string;
+  body: string;
 }
 
-export default App
+const App = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchPosts = async () => {
+    try {
+      setLoading(true);
+      const data = await ky
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .json();
+      setPosts(data as Post[]);
+      setError(null);
+    } catch (err) {
+      const message = err instanceof Error ? err.message : "error";
+      setError(message);
+      console.error("Ошибпка при загрузке постов:", message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+    const intervalId = setInterval(fetchPosts, 5000);
+    debugger;
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Posts:</h1>
+
+      {loading && !posts.length && <p>Загрузка постов...</p>}
+      {error && <p className="error">Ошибка: {error}</p>}
+
+      <div className="posts-container">
+        {posts.map((post) => (
+          <article key={`post-${post.id}`} className="post">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </article>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default App;
